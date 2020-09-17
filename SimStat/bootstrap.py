68a6711ad14f0d1d5dd613091967b292f612a75c
mostafa.safaie <mostafa.safaie@gmail.com>
2020-09-17 23:49:25 +0200

    fix insig p-val

--- SimStat/bootstrap.py
+++ SimStat/bootstrap.py
@@ -66,6 +66,7 @@ def bootstrapTest(data,n=10000):
     b_data=bootstrap(data[~np.isnan(data)],n)
     CI=np.nanpercentile(b_data,[5,95])
     
+    p=1
     if np.prod(CI) >0:
         N=len(b_data[b_data<0]) if CI[0]>0 else len(b_data[b_data>0])
         p=N/n
@@ -73,7 +74,7 @@ def bootstrapTest(data,n=10000):
 
 
 if __name__ == '__main__':
-    data=np.random.normal(loc=1, scale=.5, size=150)
+    data=np.random.normal(loc=1, scale=3, size=150)
     
     p=bootstrapTest(data,n=10000)
     print(p)
