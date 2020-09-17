d01bcd5ff2ec3be88e8813a3d253439532c7001d
mostafa.safaie <mostafa.safaie@gmail.com>
2020-09-17 11:40:07 +0200

    review permTest

--- SimStat/permTest.py
+++ SimStat/permTest.py
@@ -1,47 +1,50 @@
-class TwoTailPermTest:
+import numpy as np
+class permTest:
     """
-    Permutation test as to whether there is significant difference between group one and two.
+    A general-purpose permutation test, based on "Fujisawa 2008 Nature Neuroscience"
+    To test whether there is a difference between group one and two.
     
-    group1, group2: Represent the data. they could be either one dimentional (several realizations)
-        or 2-D (several realizaions through out the time/space/... course)
-        EX: x.shape==(15,500) means 15 trials/samples over 500 time bins
+    group1, group2: Represent the data.
+                    They could be either 1D (several realizations)
+                    or 2D (several realisaions through out the time/space/... course)
+    
+    EX: x.shape==(15,500) means 15 trials/samples over 500 time bins
 
     nIterations: Number of iterations used to shuffle. max(iterN)=(len(x)+len(y))!/len(x)!len(y)!
 
-    initGlobConfInterval:
-        Initial value for the global confidence band.
+    initGlobConfInterval: Initial value for the global confidence band.
 
-    smoothSigma: the standard deviation of the gaussian kernel used for smoothing when there are multiple data points,
-        based on the Fujisawa 2008 paper, default value: 0.05
+    smoothSigma: the standard deviation of the gaussian kernel used for smoothing when there are multiple data points, default value: 0.05
 
     Outputs:
         pVal: P-values
         highBand, lowBand: AKA boundary. Represents global bands.
-        significantDiff: An array of True or False, indicating whether there is a difference.
+        significantDiff: A binary array, indicating whether there is a difference.
     
+    based on code by M. A. Sharbaf
     """  
-    def __init__(self, group1, group2, nIterations=1000, initGlobConfInterval=5, smoothSigma=0.05, randomSeed=1):
-        self.__group1, self.__group2 = self.__setGroupData(group1), self.__setGroupData(group2)
-        self.__nIterations, self.__smoothSigma = nIterations, smoothSigma
-        self.__initGlobConfInterval = initGlobConfInterval
-        self.__randomSeed = randomSeed
+    def __init__(self, group1, group2, nIterations=10000, initGlobConfInterval=5, smoothSigma=0.05, randomSeed=1):
+        self.group1, self.group2 = self.setGroupData(group1), self.setGroupData(group2)
+        self.nIterations, self.smoothSigma = nIterations, smoothSigma
+        self.initGlobConfInterval = initGlobConfInterval
+        self.randomSeed = randomSeed
         
-        self.__checkGroups()
+        self.checkGroups()
 
-        # origGroupDiff is also known as D0 in the definition of permutation test.
-        self.__origGroupDiff = self.__computeGroupDiff(group1, group2)
+        # origGroupDiff is also known as D0 in the paper.
+        self.origGroupDiff = self.computeGroupDiff(group1, group2)
 
         # Generate surrogate groups, compute difference of mean for each group, and put in a matrix.
-        self.__diffSurGroups = self.__setDiffSurrGroups()
+        self.diffSurGroups = self.setDiffSurrGroups()
 
         # Set statistics
-        self.pVal = self.__setPVal()
-        self.highBand, self.lowBand = self.__setBands()
-        self.pairwiseHighBand = self.__setPairwiseHighBand()
-        self.pairwiseLowBand = self.__setPairwiseLowBand()
-        self.significantDiff = self.__setSignificantGroup()
+        self.pVal = self.setPVal()
+        self.highBand, self.lowBand = self.setBands()
+        self.pairwiseHighBand = self.etPairwiseHighBand()
+        self.pairwiseLowBand = self.setPairwiseLowBand()
+        self.significantDiff = self.setSignificantGroup()
 
-    def __setGroupData(self, groupData):
+    def setGroupData(self, groupData):
         if not isinstance(groupData, dict):
             return groupData
 
@@ -57,104 +60,104 @@ class TwoTailPermTest:
 
         return dataMat
 
-    def __checkGroups(self):
+    def checkGroups(self):
         # input check
-        if not isinstance(self.__group1, np.ndarray) or not isinstance(self.__group2, np.ndarray):
+        if not isinstance(self.group1, np.ndarray) or not isinstance(self.group2, np.ndarray):
             raise ValueError("In permutation test, \"group1\" and \"group2\" should be numpy arrays.")
 
-        if self.__group1.ndim > 2 or self.__group2.ndim > 2:
-            raise ValueError('In permutation test, the groups must be either vectors or matrices.')
+        if self.group1.ndim > 2 or self.group2.ndim > 2:
+            raise ValueError('In permutation test, the groups must be either vectors or 2D matrices.')
 
-        elif self.__group1.ndim == 1 or self.__group2.ndim == 1:
-            self.__group1 = np.reshape(self.__group1, (len(self.__group1), 1))
-            self.__group2 = np.reshape(self.__group2, (len(self.__group2), 1))
+        elif self.group1.ndim == 1 or self.group2.ndim == 1:
+            self.group1 = np.reshape(self.group1, (len(self.group1), 1))
+            self.group2 = np.reshape(self.group2, (len(self.group2), 1))
 
-    def __computeGroupDiff(self, group1, group2):
+    def computeGroupDiff(self, group1, group2):
         meanDiff = np.nanmean(group1, axis=0) - np.nanmean(group2, axis=0)
         
-        if len(self.__group1[0]) == 1 and len(self.__group2[0]) == 1:
+        if len(self.group1[0]) == 1 and len(self.group2[0]) == 1:
             return meanDiff
         
-        return smooth(meanDiff, sigma=self.__smoothSigma)
+        return smooth(meanDiff, sigma=self.smoothSigma)
 
-    def __setDiffSurrGroups(self):
+    def  setDiffSurrGroups(self):
         # Fix seed 
-        np.random.seed(seed=self.__randomSeed)
+        np.random.seed(seed=self.randomSeed)
         # shuffling the data
-        self.__concatenatedData = np.concatenate((self.__group1,  self.__group2), axis=0)
+        self.concatenatedData = np.concatenate((self.group1,  self.group2), axis=0)
         
-        diffSurrGroups = np.zeros((self.__nIterations, self.__group1.shape[1]))
-        for iteration in range(self.__nIterations):
+        diffSurrGroups = np.zeros((self.nIterations, self.group1.shape[1]))
+        for iteration in range(self.nIterations):
             # Generate surrogate groups
             # Shuffle every column.
-            np.random.shuffle(self.__concatenatedData)  
+            np.random.shuffle(self.concatenatedData)  
 
             # Return surrogate groups of same size.            
-            surrGroup1, surrGroup2 = self.__concatenatedData[:self.__group1.shape[0], :], self.__concatenatedData[self.__group1.shape[0]:, :]
+            surrGroup1, surrGroup2 = self.concatenatedData[:self.group1.shape[0], :], self.concatenatedData[self.group1.shape[0]:, :]
             
             # Compute the difference between mean of surrogate groups
-            surrGroupDiff = self.__computeGroupDiff(surrGroup1, surrGroup2)
+            surrGroupDiff = self.computeGroupDiff(surrGroup1, surrGroup2)
             
             # Store individual differences in a matrix.
             diffSurrGroups[iteration, :] = surrGroupDiff
 
         return diffSurrGroups
  
-    def __setPVal(self):
-        positivePVals = np.sum(1*(self.__diffSurGroups > self.__origGroupDiff), axis=0) / self.__nIterations
-        negativePVals = np.sum(1*(self.__diffSurGroups < self.__origGroupDiff), axis=0) / self.__nIterations
+    def setPVal(self):
+        positivePVals = np.sum(1*(self.diffSurGroups > self.origGroupDiff), axis=0) / self.nIterations
+        negativePVals = np.sum(1*(self.diffSurGroups < self.origGroupDiff), axis=0) / self.nIterations
         return np.array([np.min([1, 2*pPos, 2*pNeg]) for pPos, pNeg in zip(positivePVals, negativePVals)])
 
-    def __setBands(self):
-        if not isinstance(self.__origGroupDiff, np.ndarray):  # single point comparison
+    def setBands(self):
+        if not isinstance(self.origGroupDiff, np.ndarray):  # single point comparison
             return None, None
         
         alpha = 100 # Global alpha value
-        highGlobCI = self.__initGlobConfInterval  # global confidance interval
-        lowGlobCI = self.__initGlobConfInterval  # global confidance interval
+        highGlobCI = self.initGlobConfInterval  # global confidance interval
+        lowGlobCI = self.initGlobConfInterval  # global confidance interval
         while alpha >= 5:
-            # highBand = np.percentile(a=self.__diffSurGroups, q=100-highGlobCI, axis=0)
-            # lowBand = np.percentile(a=self.__diffSurGroups, q=lowGlobCI, axis=0)
+            # highBand = np.percentile(a=self.diffSurGroups, q=100-highGlobCI, axis=0)
+            # lowBand = np.percentile(a=self.diffSurGroups, q=lowGlobCI, axis=0)
 
-            highBand = np.percentile(a=self.__diffSurGroups, q=100-highGlobCI)
-            lowBand = np.percentile(a=self.__diffSurGroups, q=lowGlobCI)
+            highBand = np.percentile(a=self.diffSurGroups, q=100-highGlobCI)
+            lowBand = np.percentile(a=self.diffSurGroups, q=lowGlobCI)
 
             breaksPositive = np.sum(
-                [np.sum(self.__diffSurGroups[i, :] > highBand) > 1 for i in range(self.__nIterations)]) 
+                [np.sum(self.diffSurGroups[i, :] > highBand) > 1 for i in range(self.nIterations)]) 
             
             breaksNegative = np.sum(
-                [np.sum(self.__diffSurGroups[i, :] < lowBand) > 1 for i in range(self.__nIterations)])
+                [np.sum(self.diffSurGroups[i, :] < lowBand) > 1 for i in range(self.nIterations)])
             
-            alpha = ((breaksPositive + breaksNegative) / self.__nIterations) * 100
+            alpha = ((breaksPositive + breaksNegative) / self.nIterations) * 100
             highGlobCI = 0.95 * highGlobCI
             lowGlobCI = 0.95 * lowGlobCI
         return highBand, lowBand           
 
-    def __setSignificantGroup(self):
-        if not isinstance(self.__origGroupDiff, np.ndarray):  # single point comparison
+    def setSignificantGroup(self):
+        if not isinstance(self.origGroupDiff, np.ndarray):  # single point comparison
             return self.pVal <= 0.05
 
         # finding significant bins
-        globalSig = np.logical_or(self.__origGroupDiff > self.highBand, self.__origGroupDiff < self.lowBand)
-        pairwiseSig = np.logical_or(self.__origGroupDiff > self.__setPairwiseHighBand(), self.__origGroupDiff < self.__setPairwiseLowBand())
+        globalSig = np.logical_or(self.origGroupDiff > self.highBand, self.origGroupDiff < self.lowBand)
+        pairwiseSig = np.logical_or(self.origGroupDiff > self.setPairwiseHighBand(), self.origGroupDiff < self.setPairwiseLowBand())
         
         significantGroup = globalSig.copy()
         lastIndex = 0
         for currentIndex in range(len(pairwiseSig)):
             if (globalSig[currentIndex] == True):
-                lastIndex = self.__setNeighborsToTrue(significantGroup, pairwiseSig, currentIndex, lastIndex)
+                lastIndex = self.setNeighborsToTrue(significantGroup, pairwiseSig, currentIndex, lastIndex)
 
         return significantGroup
     
-    def __setPairwiseHighBand(self, localBandValue=0.5):        
-        return np.percentile(a=self.__diffSurGroups, q=100 - localBandValue, axis=0)
+    def setPairwiseHighBand(self, localBandValue=0.5):        
+        return np.percentile(a=self.diffSurGroups, q=100 - localBandValue, axis=0)
 
-    def __setPairwiseLowBand(self, localBandValue=0.5):        
-        return np.percentile(a=self.__diffSurGroups, q=localBandValue, axis=0)
+    def setPairwiseLowBand(self, localBandValue=0.5):        
+        return np.percentile(a=self.diffSurGroups, q=localBandValue, axis=0)
 
-    def __setNeighborsToTrue(self, significantGroup, pairwiseSig, currentIndex, previousIndex):
+    def setNeighborsToTrue(self, significantGroup, pairwiseSig, currentIndex, previousIndex):
         """
-            While the neighbors of a global point pass the local band (consecutively), set the global band to true.
+            While the neighbors of a global point pass the local band, set the global band to true.
             Returns the last index which was set to True.
         """ 
         if (currentIndex < previousIndex):
@@ -175,24 +178,3 @@ class TwoTailPermTest:
                 break
         
         return previousIndex
-    
-    def plotSignificant(self,ax: plt.Axes.axes,y: float,x=None,**kwargs):
-        if x is None:
-            x=np.arange(0,len(self.significantDiff))+1
-        for x0,x1,p in zip(x[:-1],x[1:],self.significantDiff):
-            if p:
-                ax.plot([x0,x1],[y,y],zorder=-2,**kwargs)
-                
-    @staticmethod
-    def plotSigPair(ax: plt.Axes.axes,y: float,x=None, s: str ='*',**kwargs):
-        if x is None:
-            x=(0,len(self.significantDiff))
-        if 'color' not in kwargs:
-            kwargs['color']='k'
-        
-        dy=.03*(ax.get_ylim()[1]-ax.get_ylim()[0])
-        ax.plot(x,[y,y],**kwargs)
-        ax.plot([x[0],x[0]],[y-dy,y],[x[1],x[1]],[y-dy,y],**kwargs)
-        ax.text(np.mean(x),y,s=s,
-                ha='center',va='center',color=kwargs['color'],
-                size='xx-small',fontstyle='italic',backgroundcolor='w')
